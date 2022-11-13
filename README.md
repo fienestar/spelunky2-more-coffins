9555963cff2d2fc7b9405a7544e22739afd96998
fienestar <fienestar@gmail.com>
2022-11-13 19:04:15 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# [More Coffins](https://spelunky.fyi/mods/m/more-coffins/)
+Add coffins to 7-30, 7-70.
+
+Install code: `more-coffins`
+
+Therefore, if there is a dead person, coffins are created at 7-10, 30, 50, 70, 90)
+
+Caution: all participants in the game must use this mode.
---
+++ main.lua
@@ -0,0 +1,54 @@
+meta.name = 'More Coffins'
+meta.version = '0.0.2'
+meta.description = 'add coffins to 7-30, 7-70'
+meta.author = 'fienestar'
+meta.online_safe = true
+
+local function pick(from)
+    return from[prng:random(#from)]
+end
+
+last_coffin = {0,0}
+
+set_callback(function(ctx)
+    if state.level == 30 or state.level == 70 then
+        -- the number of orb always zero in POST_ROOM_GENERATION
+        -- #get_entities_by_type(ENT_TYPE.ITEM_FLOATING_ORB)
+
+        if get_room_template(last_coffin[1], last_coffin[2], LAYER.FRONT) == ROOM_TEMPLATE.COFFIN_PLAYER_VERTICAL then
+            return
+        end
+
+        local coffin_candidates = {}
+        for rx=0,state.width-1 do
+            for ry=0,state.height-1 do
+                if get_room_template(rx, ry, LAYER.FRONT) == ROOM_TEMPLATE.SIDE then
+                    table.insert(coffin_candidates, {rx, ry})
+                end
+            end
+        end
+
+        local need_coffin_count = 0
+        for i=1, state.items.player_count do
+            if state.items.player_inventory[i].health == 0 then
+                need_coffin_count = need_coffin_count + 1
+            end
+        end
+
+        if need_coffin_count > #coffin_candidates then
+            need_coffin_count = #coffin_candidates
+        end
+
+        for i=0, need_coffin_count-1 do
+            local pos = pick(coffin_candidates)
+            while get_room_template(pos[1], pos[2], LAYER.FRONT) ~= ROOM_TEMPLATE.SIDE do
+                pos = pick(coffin_candidates)
+            end
+
+            -- coffin code for CO is COFFIN_PLAYER_VERTICAL
+            -- NOT COFFIN_PLAYER
+            last_coffin = pos
+            ctx:set_room_template(pos[1], pos[2], LAYER.FRONT, ROOM_TEMPLATE.COFFIN_PLAYER_VERTICAL)
+        end
+    end
+end, ON.POST_ROOM_GENERATION)
